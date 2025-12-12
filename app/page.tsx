6f454bc4ff710a78b2169b49a7d5772d684e7740
Lychee 🖤 <rambutanandlycheejuice@example.com>
2025-12-12 15:39:06 -0500

    Completed intro part of the project

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,30 @@ import initPlanet3D from "@/components/3D/planet";
 
 export default function Home() {
   useEffect(() => {
-    initPlanet3D();
+    const { scene, renderer } = initPlanet3D();
+
+    return () => {
+      if (renderer) {
+        const gl = renderer.getContext();
+        gl.getExtension("WEBGL_lose_context")?.loseContext();
+        renderer.dispose();
+      }
+    };
   }, []);
+
   return (
     <div className="page">
       <section className="hero_main">
+        <div className="content">
+          <h1>Welcome To The New World</h1>
+
+          <p>
+            AI agents that actually bring value to businesses and elevate
+            workers productivity.
+          </p>
+
+          <button className="cta_btn">Get started.</button>
+        </div>
         <canvas className="planet-3D" />
       </section>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.glsl$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+  turbopack: {},
 };
 
 export default nextConfig;
